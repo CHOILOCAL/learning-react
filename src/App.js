c9010435d3126ee17392bcf0c893f78547183138
CHOILOCAL <giants1217@naver.com>
2021-09-23 12:04:47 +0900

    9.2.2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
-import React, {Component} from 'react';
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
+import React from 'react';
+import SassComponent from "./SassComponent";
+// import Info from "./Info";
+// import Average from "./Average";
+// import Counter from "./Counter";
+// import ErrorBoundary from "./ErrorBoundary";
 
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString((16));
-}
+// function getRandomColor() {
+//     return '#' + Math.floor(Math.random() * 16777215).toString((16));
+// }
 
-class App extends Component {
+// class App extends Component {
 
-    state = {
-        color: '#000000'
-    }
+    // state = {
+    //     color: '#000000'
+    // }
 
-    handleClick = () => {
-        this.setState({
-            color: getRandomColor()
-        });
-    }
+    // handleClick = () => {
+    //     this.setState({
+    //         color: getRandomColor()
+    //     });
+    // }
 
-    render() {
-        return (
-            <>
-            <button onClick={this.handleClick}>랜덤색상</button>
-            <ErrorBoundary>
-                <LifeCycleSample color={this.state.color} />
-            </ErrorBoundary>
-            </>
+    // render() {
+    //     return (
+    //         <>
+// <button onClick={this.handleClick}>랜덤색상</button>*/}
+            // <ErrorBoundary>
+            //     <Info />
+                // <LifeCycleSample color={this.state.color} />*/}
+            // </ErrorBoundary>
+            // </>
             // <div>
             //     <ScrollBox ref={(ref) => this.scrollBox=ref}/>
             //     <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
             // </div>
-        );
-    }
-}
+        // );
+    // }
+// }
+
+// export default App;
+
+
+const App = () => {
+                // const [visible, setVisible] = useState(false);
+    return (
+        <div>
+            <SassComponent />
+            {/*<Info />*/}
+            {/*<Counter />*/}
+            {/*<button onClick={() => {*/}
+            {/*    setVisible(!visible);*/}
+            {/*}}>{visible ? '숨기기' : '보이기'}</button>*/}
+            {/*<hr />*/}
+            {/*{visible && <Info />}*/}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,48 @@
+import React  from 'react';
+import useInputs from "./useInputs";
+
+const Info = () => {
+    const [state, onChange] = useInputs({
+        name: '',
+        nickname: ''
+    });
+
+    const { name, nickname } = state;
+
+    // const [name, setName] = useState("");
+    // const [nickname, setNickname] = useState("");
+
+    // useEffect(() => {
+    //     console.log(`effect`);
+    //     console.log(name);
+    //     return () => {
+    //         console.log(`clean up`);
+    //         console.log(name);
+    //     }
+    // }, []);
+
+    // const onChangeName = e => {
+    //     setName(e.target.value);
+    // }
+
+    // const onChangeNickname = e => {
+    //     setNickname(e.target.value);
+    // }
+
+    return (
+        <div>
+            <div>
+                <input name="name" value={name} onChange={onChange}/>
+                <input name="nickname" value={nickname} onChange={onChange}/>
+            </div>
+            <div>
+                <b>이름:</b> {name}
+            </div>
+            <div>
+                <b>닉네임:</b> {nickname}
+            </div>
+        </div>
+    );
+};
+
+export default Info;
